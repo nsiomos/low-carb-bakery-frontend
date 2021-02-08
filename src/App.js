5ab5e5f94ff7d9b7c37164cfa710b7a32c2409fb
Nicole Siomos <nicole.siomos@yahoo.com>
2021-02-08 16:17:29 +0100

    - Added fddb schema and rudimentary validation. - Added ajv dependency. - Validation is, for now, done in console out.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 import "./styles.css";
+import Ajv from "ajv";
 
 export default function App() {
+  var fddbSchema = require("./fddb.schema.json");
+  var fddb = require("./fddb.json");
+
+  const ajv = new Ajv(); // options can be passed, e.g. {allErrors: true}
+  const validate = ajv.compile(fddbSchema);
+  const valid = validate(fddb);
+  if (!valid) console.log(validate.errors);
+
   return (
     <div className="App">
       <h1>Hello low-carb-bakery frontend</h1>
