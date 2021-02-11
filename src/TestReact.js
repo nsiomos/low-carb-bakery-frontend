54d568e4e52b0065bf7d64c588201ca6b557e346
Nicole Siomos <nicole.siomos@yahoo.com>
2021-02-11 07:54:35 +0100

    - Added missing purchase information to fddb.

--- src/TestReact.js
+++ src/TestReact.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class TestReact extends React.Component {
   render() {
-    return <input className="testReact" value={this.props.value} />;
+    return <input className="testReact" value={this.props.value} readOnly />;
   }
 }
 
